66d656d3fed8af80d0f445370c2e69380766ebb6
Hayden Le <4268811+haydlite@users.noreply.github.com>
2020-06-17 21:04:48 -0400

    updated -- some linting + handled some exceptions

--- main.py
+++ main.py
@@ -3,95 +3,122 @@
 Scraping YouTube Videoes for Set of Metadata
 """
 
-# TODO: Sometimes URLs differ in format
-
-# TODO: Title CHECK
-# TODO: Creator CHECK
-# TODO: Views as of Date
-# TODO: Date Uploaded
-# TODO: Video Length
-
-# TODO: Test
-    
+# TODO: Determine all URL types
+# TODO: Determine why 'schema.org'?
 
 import pandas as pd
+
+import csv
 import requests
+import sys
+
 from bs4 import BeautifulSoup
 
-YOUTUBE_VIDEO_PREFIX = 'https://www.youtube.com/watch?v='
-wdir = '/Users/hle/projects/youtube-metadata-scrape/'
+RELEVANT_METADATA = ['ownerChannelName',
+                     'title',
+                     'viewCount',
+                     'uploadDate',
+                     'approxDurationMs'
+                     ]
 
 
-def video_code(youtube_url): # shorten URL only
-    return youtube_url[youtube_url.rindex('/')+1:]
-
-
-def lengthen_url(youtube_url):
-    return YOUTUBE_VIDEO_PREFIX + video_code(youtube_url)
-
-
-def find_metadata_variable_value(soup, metadata_variable_to_find):
-    variable_identifier_string = '{}\\":\\"'.format(metadata_variable_to_find)
-    variable_start_index = soup.find_all('script')[13].text.\
-        rfind('{}\\":\\"'.format(metadata_variable_to_find)) 
-        + len('{}\\":\\"'.format(metadata_variable_to_find))
+class YouTubeScraper:
+    YOUTUBE_VIDEO_PREFIX = 'https://www.youtube.com/watch?v='
+    
+    @classmethod
+    def video_code(self, youtube_url): # shorten URL only
+        return youtube_url[youtube_url.rindex('/')+1:]
+    
+    
+    @classmethod
+    def lengthen_url(self, youtube_url):
+        return self.YOUTUBE_VIDEO_PREFIX + self.video_code(youtube_url)
+    
+    
+    @classmethod
+    def find_metadata_variable_value(self, soup, metadata_variable_to_find, alt=False):
+        variable_identifier_string = '{}\\":\\"'.format(metadata_variable_to_find)
+        variable_identifier_offset = len(variable_identifier_string)
         
-    metadata_string = soup.find_all('script')[13].text
-    metadata_string_cut = metadata_string[variable_start_index:]
+        if alt:
+            relevant_html_str = soup.find_all('script')[12].text
+        else:
+            relevant_html_str = soup.find_all('script')[13].text
+        
+        variable_start_index = relevant_html_str.rfind(variable_identifier_string) + \
+                               variable_identifier_offset
+            
+        relevant_html_str_cut = relevant_html_str[variable_start_index:]
+        
+        variable_end_index = relevant_html_str_cut.find('\\"')
+        variable_value = relevant_html_str_cut[:variable_end_index]
+        
+        return variable_value
     
-    variable_end_index = metadata_string_cut.find('\\"')
-    variable_value = metadataStringCut[:variable_end_index]
     
-    return variable_value
+def null_row_values(url):
+    return {'URL': url, 
+            'Title': 'N/A', 
+            'Creator': 'N/A', 
+            'View Count': 'N/A', 
+            'Upload Date': 'N/A',
+            'Video Length (MS)': 'N/A',
+            'Video Length (minutes)': 'N/A'}
 
-     
-
-def find_title(soup):
-    title_StartIndex = soup.find_all('script')[13].text.rfind('viewCount\\":\\"') + len('viewCount\\":\\"')
-    metadataString = soup.find_all('script')[13].text
-    metadataStringCut = metadataString[title_StartIndex:]
-    title_EndIndex = metadataStringCut.find('\\"')
-    title = metadataStringCut[:title_EndIndex]
-    return title
-
-
-def find_creator(soup):
-    ownerChannelName_StartIndex = soup.find_all('script')[13].text.rfind('ownerChannelName\\":\\"') + len('ownerChannelName\\":\\"')
-    metadataString = soup.find_all('script')[13].text
-    metadataStringCut = metadataString[ownerChannelName_StartIndex:]
-    ownerChannelName_EndIndex = metadataStringCut.find('\\"')
-    ownerChannelName = metadataStringCut[:ownerChannelName_EndIndex]
-    return ownerChannelName
-
-def find_view_count(soup):
-    viewCount_StartIndex = soup.find_all('script')[13].text.rfind('viewCount\\":\\"') + len('viewCount\\":\\"')
-    metadataString = soup.find_all('script')[13].text
-    metadataStringCut = metadataString[viewCount_StartIndex:]
-    viewCount_EndIndex = metadataStringCut.find('\\"')
-    viewCount = metadataStringCut[:viewCount_EndIndex]
-    return viewCount
-
-
-df = pd.read_excel('youtube_url.xlsx')
-url = df.video_url[50]
-if url.rfind('=') == -1: # If URL is shorten form
-    url = lengthen_url(url)
-
-response = requests.get(url, headers={ 'User-Agent': 'Mozilla/5.0' })
-
-soup = BeautifulSoup(response.text, features="html.parser")
-creator = find_creator(soup)
-title = find_title(soup)
-
-
-
-
- 
-with open(wdir + '{}.txt'.format(url[url.rfind('=')+1:]), 'w+', encoding='utf-8') as f_out:
-    f_out.write(soup.prettify())
     
+if __name__ == "__main__":
+    urls_to_scrape = pd.read_excel('youtube_url.xlsx').video_url
+    
+    with open('youtube_videos_metadata.csv', mode='w') as csv_file:
+        field_names = ['URL', 'Title', 'Creator', 'View Count', 'Upload Date',
+                       'Video Length (MS)', 'Video Length (minutes)']
+        writer = csv.DictWriter(csv_file, fieldnames=field_names)
+        writer.writeheader()
+    
+        for url in urls_to_scrape:
+            if 'youtu' in url:
+                if '-' not in url: # If URL is shortened form
+                    url = YouTubeScraper.lengthen_url(url)
+                elif '&' in url:
+                    ampersand_index = url.find('&')
+                    url = url[:ampersand_index]
+                    
+                response = requests.get(url, headers={ 'User-Agent': 'Mozilla/5.0' })
+                soup = BeautifulSoup(response.text, features="html.parser")
+                metadata_values = {}
+                is_alt = False
+                
+                for var in RELEVANT_METADATA:
+                    metadata_values[var] = YouTubeScraper.find_metadata_variable_value(soup, var, is_alt)
+                    if (('https://schema.org' in metadata_values[var]) or \
+                       ('http://schema.org'  in metadata_values[var])):
+                        is_alt = True
+                        metadata_values[var] = YouTubeScraper.find_metadata_variable_value(soup, var, is_alt)
+                    
+                if (not metadata_values['viewCount']) or \
+                   ('window' in metadata_values['viewCount']): # check if video is removed
+                    row_values = null_row_values(url)
+                else:
+                    try:
+                        video_length_minutes = float(metadata_values['approxDurationMs']) * 1.66666666e-5
+                        
+                        row_values = {'URL': url, 'Title': metadata_values['title'], 
+                            'Creator': metadata_values['ownerChannelName'], 
+                            'View Count': metadata_values['viewCount'], 
+                            'Upload Date': metadata_values['uploadDate'],
+                            'Video Length (MS)': metadata_values['approxDurationMs'], 
+                            'Video Length (minutes)': str(video_length_minutes)}
+                    except:
+                        e = sys.exc_info()[0]
+                        print( "Error: {}".format(e))
+                        print(url)
+                        print(metadata_values)
+                        
+                        row_values = null_row_values(url)
+            else: # not a youtube URL
+                row_values = null_row_values(url)
 
-
+            writer.writerow(row_values)
 
 # title = find_title(page)
 
